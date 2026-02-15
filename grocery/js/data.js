27e357da95fd90e75380e9363d4438d11881dd0b
pandeyArushi9 <159776315+pandeyArushi9@users.noreply.github.com>
2026-02-15 14:52:55 +0545

    Delete grocery directory

--- grocery/js/data.js
+++
@@ -1,6 +0,0 @@
-export const groceryItems = [
-  { id: "1", name: "milk", completed: true },
-  { id: "2", name: "bread", completed: true },
-  { id: "3", name: "eggs", completed: false },
-  { id: "4", name: "butter", completed: false },
-];
